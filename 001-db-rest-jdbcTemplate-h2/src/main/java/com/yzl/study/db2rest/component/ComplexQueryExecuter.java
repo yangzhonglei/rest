d4b276b56c30a83faa7a9fada04b580d19f28e68
杨中磊 <yangzhonglei@yupont.com>
2020-07-13 10:52:57 +0800

    简化count

--- 001-db-rest-jdbcTemplate-h2/src/main/java/com/yzl/study/db2rest/component/ComplexQueryExecuter.java
+++ 001-db-rest-jdbcTemplate-h2/src/main/java/com/yzl/study/db2rest/component/ComplexQueryExecuter.java
@@ -10,12 +10,15 @@ import org.springframework.jdbc.core.RowCallbackHandler;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import com.yzl.study.db2rest.annotation.SimpleRestAspect;
 import com.yzl.study.db2rest.model.ComplexQuery;
 import com.yzl.study.db2rest.model.Order;
 import com.yzl.study.db2rest.model.Page;
 import com.yzl.study.db2rest.model.Sort;
 import com.yzl.study.db2rest.utils.FieldNameConvertor;
 
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
 @Component
 public class ComplexQueryExecuter {
 
@@ -25,13 +28,13 @@ public class ComplexQueryExecuter {
 	public Integer count(ComplexQuery c, Map<String, Object> paraMap) {
 		try {
 			String sql = c.getSql();
-			String countSql = "";
-			if (sql.toLowerCase().contains("from")) {
-
-				int i = sql.toLowerCase().indexOf("from");
-				countSql = " select count(1) " + sql.substring(i);
-
-			}
+			String countSql = " SELECT count(1) from ( "+ sql +" ) tmpXXXXXXXXXX " ;
+//			if (sql.toLowerCase().contains("from")) {
+//
+//				int i = sql.toLowerCase().indexOf("from");
+//				countSql = " select count(1) " + sql.substring(i);
+//
+//			}
 			return namedParameterJdbcTemplate.queryForObject(countSql, paraMap, Integer.class);
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -46,6 +49,16 @@ public class ComplexQueryExecuter {
 			int pageNo = page.getPage();
 			int pageSize = page.getSize();
 			String sql = c.getSql();
+			StringBuilder sqlSb = new StringBuilder();
+			//sql 
+//			int index = sql.indexOf("<if");
+//			int start = 0;
+//			while(index >-1) {
+//				sqlSb=sqlSb.append(sql.substring(start, index));
+//				int index2 = sql.indexOf(">", index+3);
+//				
+//				sql.indexOf(str, fromIndex)
+//			}
 			StringBuilder sb = new StringBuilder();
 			sb.append(sql);
 			Sort st = page.getSort();
@@ -53,14 +66,20 @@ public class ComplexQueryExecuter {
 
 				StringBuilder orderByStr = new StringBuilder();
 				for (Order o : st.getOrders()) {
-					orderByStr.append(o.getColName() + " " + o.getSortType() + ",");
+					if(o.isGbkconvert()) {
+						orderByStr.append(" convert("+ o.getColName()+" using gbk) "+ o.getSortType()+",");
+					}else {
+						
+						orderByStr.append(o.getColName() + " " + o.getSortType() + ",");
+					}
 				}
 				orderByStr.deleteCharAt(orderByStr.length() - 1);
 				sb.append(" order by " + orderByStr.toString());
 			}
-			sb.append(" limit " + ((pageNo - 1) * pageSize) + " , " + pageNo * pageSize);
-			System.out.println("======================" + sb.toString());
-			List<Map<String, Object>> queryForList = namedParameterJdbcTemplate.queryForList(sb.toString(), paraMap);
+			sb.append(" limit " + ((pageNo - 1) * pageSize) + " , " + pageSize);
+			String toBeExeSql = sb.toString();
+			log.debug("===to be execute sql:{}",toBeExeSql);
+			List<Map<String, Object>> queryForList = namedParameterJdbcTemplate.queryForList(toBeExeSql, paraMap);
 			return FieldNameConvertor.columnName2FieldName(queryForList);
 
 		} catch (Exception e) {
